83697a3c877677b2dcea27b7d25274f5fe0995d6
falskuyyy <ahmdfalah10@gmail.com>
2025-01-26 03:24:27 +0700

    Membuat data class buku

---
+++ app/src/main/java/com/example/uaspam/model/buku.kt
@@ -0,0 +1,17 @@
+package com.example.uaspam.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Buku (
+    val id_buku: Int,
+    val nama_buku: String,
+    val deskripsiBuku: String,
+    val tanggalTerbit: String,
+    val statusBuku: String,
+    val id_kategori: Int,
+    val id_penulis: Int,
+    val id_penerbit: Int,
+)
+
+
