57bcf7d57d3b2292e1d4238e9c34b11883e0308b
falskuyyy <ahmdfalah10@gmail.com>
2025-01-26 02:20:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uaspam/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.uaspam
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.uaspam.ui.theme.UasPAMTheme
+import com.example.uaspam.ui.viewmodel.BukuApp
+
+class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UasPAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BukuApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
